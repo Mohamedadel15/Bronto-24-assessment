3d1c7d62807f8f4b4a0e47c54992b06327eb01c5
Mohamedadel15 <mibo82239@gmail.com>
2024-07-29 16:13:34 -0700

    new change

--- src/app/loadng.tsx
+++ src/app/loadng.tsx
@@ -1,6 +1,5 @@
 import LoadingPage from "@/components/loading/loadingPage";
 
 export default function Loading() {
-    // You can add any UI inside Loading, including a Skeleton.
     return <LoadingPage />;
 }
