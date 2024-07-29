06a4d019890e84222b3a75f366de57c6f534bd19
Mohamedadel15 <mibo82239@gmail.com>
2024-07-29 16:05:13 -0700

    add page error , loading and not found

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,28 +13,31 @@ interface User {
 }
 
 export default async function HomePage() {
-  let users = await GetDataInServerSide("/userprofiles/", {
-    cache: "no-store",
-  });
+  try {
+    let users = await GetDataInServerSide("/userprofiles/", {
+      cache: "no-store",
+    });
 
-  return (
-    <article className="min-h-screen px-6 py-8 flex flex-col gap-5">
-      <div className="flex justify-end w-full">
-        <UserBtn addUser />
-      </div>
-      {
-        users?.count == 0 ? (
-          <p className="text-center font-bold text- 3xl text-gray-400 min-h-screen flex items-center justify-center">No users found</p>
-        ) : (
-          <section className="grid grid-cols-custom-300 gap-4 place-items-center">
-            {
-              users?.results?.map((user: User) => (
-                <UserCard key={user.id} user_details={user} />
-              ))
-            } 
-          </section>)
-      }
-
-    </article>
-  );
+    return (
+      <article className="min-h-screen px-6 py-8 flex flex-col gap-5">
+        <div className="flex justify-end w-full">
+          <UserBtn addUser />
+        </div>
+        {
+          users?.count == 0 ? (
+            <p className="text-center font-bold text- 3xl text-gray-400 min-h-screen flex items-center justify-center">No users found</p>
+          ) : (
+            <section className="grid grid-cols-custom-300 gap-4 place-items-center">
+              {
+                users?.results?.map((user: User) => (
+                  <UserCard key={user.id} user_details={user} />
+                ))
+              }
+            </section>)
+        }
+      </article>
+    );
+  } catch (error) {
+    return <p className="w-full h-screen text-center flex items-center justify-center text-3xl font-bold text-primary">Something went wrong</p>;
+  }
 }
